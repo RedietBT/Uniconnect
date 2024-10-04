3a7c347b6a908a4bd1ae8584f45fe0f2ec89a151
Tsedi7 <133485595+Tsedi7@users.noreply.github.com>
2024-10-04 10:40:25 -0700

    Update Home_page.vue

--- src/components/Home_page.vue
+++ src/components/Home_page.vue
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>UniConnect</title>
+        <title>UNICONNECT</title>
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     </head>
     <body>
